37d000cdf5e059d3a96f1287c1f2e71ae1332464
Travis Pritchard <pritchard.travis@deq.state.or.us>
2021-07-22 14:21:47 -0700

    DO data query script

---
+++ Parameters/DO/DO.R
@@ -0,0 +1,5 @@
+source('Parameters/DO/fun_DO_data.R')
+
+
+
+Results_censored_DO <- DO_data('IR_Dev')
---
+++ Parameters/DO/fun_DO_data.R
@@ -0,0 +1,34 @@
+
+
+DO_data <- function(database) {
+  print("Fetch DO data from IR database")
+  #connect to IR database view as a general user
+  # import Temperature data
+  IR.sql <-   odbcConnect(database)
+  
+  
+  # Get data from IR database where wqstd_code = 12, ResStatusName = Final, 
+  # Join with Crit_Temp to get temperature Criteria and spawn ?
+  Results_import <-    sqlFetch(IR.sql, "dbo.VW_DO") 
+  
+  
+  odbcClose(IR.sql)
+  
+  
+  
+  print(paste("Fetched", nrow(Results_import), "results from", length(unique(Results_import$MLocID)), "monitoring locations in",
+              length(unique(Results_import$AU_ID)), "AUs"))
+  
+  # Set factors to characters
+  Results_import %>% map_if(is.factor, as.character) %>% as_data_frame -> Results_import
+  
+  
+  
+  # Data aggregation --------------------------------------------------------
+  
+  
+  
+  
+  return(Results_import)
+  
+}
