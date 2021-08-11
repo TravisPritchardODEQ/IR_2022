d840004efa25de82d7db5e96e81847b85f19cd8b
Travis Pritchard <pritchard.travis@deq.state.or.us>
2021-08-11 07:29:27 -0700

    pentachlorphenol

---
+++ Parameters/Tox_AL/fun_ToxAL_Pentachlorophenol_assessment.R
@@ -0,0 +1,48 @@
+library(odeqIRtools)
+library(lubridate)
+
+
+TOX_AL_penta_analysis <- function(df){
+  
+  # Assign violations
+  penta_data_analysis <- df %>%
+    mutate(evaluation_crit = ifelse(WaterTypeCode == 2, pmin(CMC_crit, CCC_crit, na.rm = TRUE), 7.9 )) %>%
+    mutate(excursion = ifelse(Result_cen > evaluation_crit, 1, 0 ))
+  
+  
+  #Summarize data and assign critical excursions and IR category
+  penta_data_summary <- penta_data_analysis %>%
+    group_by(AU_ID) %>%
+    summarise(OWRD_Basin = first(OWRD_Basin),
+              num_samples = n(),
+              num_excursions = sum(excursion),
+              percent_3d = round(sum(Result_Operator == "<" & IRResultNWQSunit > evaluation_crit )/num_samples * 100)) %>%
+    mutate(critical_excursions = binomial_excursions(num_samples, type = 'Toxics'),
+           IR_category = case_when(percent_3d == 100 ~ "3D",
+                                   num_samples < 2 & num_excursions >= critical_excursions ~ "5", 
+                                   num_samples < 10  & num_excursions == 0 ~ "3",
+                                   num_samples < 10  & num_excursions > 0  ~ "3B",
+                                   num_samples >=  10 & num_excursions < critical_excursions ~ "2",
+                                   TRUE ~ "ERROR"),
+           Rationale =  case_when(percent_3d == 100 ~ paste0("All results are non-detects with detection limits above criteria- ",
+                                                                                  num_samples, " total samples"),
+                                  num_samples < 2 & num_excursions >= critical_excursions ~  paste0(num_excursions,
+                                                                                                    " excursion of criteria with ",
+                                                                                                    num_samples, " total samples. ", 
+                                                                                                    critical_excursions, " needed to list."),
+                                  num_samples < 10 & num_excursions == 0 ~ paste0("Only ",num_samples, " sample with ",
+                                                                                  num_excursions," excursions"),
+                                  num_samples < 10 & num_excursions > 0 ~ paste0("Only ",num_samples, " sample with ",
+                                                                                  num_excursions," excursion"),
+                                  
+                                  num_samples >=  10 & num_excursions < critical_excursions ~ paste0(num_excursions,
+                                                                                  " excursion of criteria with ",
+                                                                                  num_samples, " total samples. ", 
+                                                                                  critical_excursions, " needed to list."),
+                                  TRUE ~ "ERROR"))
+                                  
+
+  
+  Tox_AL_penta_list <- list(data = penta_data_analysis,
+                            penta_cats = penta_data_summary)
+}
---
+++ Parameters/Tox_AL/fun_ToxAL_Pentachlorophenol_data.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(odeqIRtools) 
+library(odbc)
+library(DBI)
+library(glue)
+
+Pentachlorophenol_data <- function(database){
+  
+  print("Fetch Pentachlorophenol data from IR database")
+  
+  #open connection to database
+  con <- DBI::dbConnect(odbc::odbc(), database)
+  
+  #Build query language to get Pentachlorophenol data out. this grabs the IR 2018 db view [dbo].[VW_Pentachlorophenol]
+  
+  db_qry <-  glue::glue_sql( "SELECT *
+  FROM [IntegratedReport].[dbo].[VW_Pentachlorophenol]", .con = con)
+  
+  # Send query to database and return with the data
+  Results_import <-  DBI::dbGetQuery(con, db_qry) 
+  
+  print(paste("Returned", nrow(Results_import), "results from", length(unique(Results_import$MLocID)), "monitoring locations"))
+  print("Fetch matching pH values")
+  
+  #Create a vector of monitoring locations with Pentachlorophenol data. This list is used as a filter for the pH query
+  mlocs <- unique(Results_import$MLocID)
+  
+  
+  #Create query for the pH data. The 'MLocID in ({mlocs*})' part automatically adds 
+  #all the mlocs vector elements to the query 
+  
+  ph_qry <- glue::glue_sql("SELECT [MLocID]
+,[SampleStartDate]
+,[IRResultNWQSunit] AS 'pH'
+,[Result_Depth]
+FROM [IntegratedReport].[dbo].[InputRaw]
+WHERE Pollu_ID = '124' AND (Statistical_Base IS NULL)
+      AND MLocID in ({mlocs*})", .con = con)
+  
+  
+  #Send to db and return with the data
+  Results_pH <- DBI::dbGetQuery(con, ph_qry)
+  
+  print("finished pH query")
+  
+  # Close database connection
+  DBI::dbDisconnect(con)
+  
+  
+  # If there are multiple pH data points in a day, we only use the first one. 
+  # This section uses first pH result for a given mloc, date, and depth
+  single_day_ph <- Results_pH %>%
+    group_by(MLocID, SampleStartDate, Result_Depth ) %>%
+    summarise(pH = first(pH)) %>%
+    ungroup()
+  
+  print("Joining pH values and calculating criteria")
+  
+  #Join the pH data to the Pentachlorophenol data and calculate the criteria
+  joined_data <- Results_import %>%
+    left_join(single_day_ph, by = c("MLocID","SampleStartDate", "Result_Depth" )) %>%
+    mutate(CMC_crit = exp(1.005*pH-4.869),
+           CCC_crit = exp(1.005*pH-5.134))
+  
+  
+  print(paste("Removing", sum(is.na(joined_data$pH)), "results with no pH values"))
+  
+  joined_data <- joined_data %>%
+    filter(!is.na(pH))
+  
+  print("Data censoring")
+  
+  #Run data censoring here
+  data_censored <- censor_data(joined_data, criteria_col = CMC_crit)
+  
+  #Return censored data from the function 
+  return(data_censored)
+  
+}
+
