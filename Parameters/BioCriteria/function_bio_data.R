1d0663c65cc26d6e53758d2d683af68cfe6c27a6
MERRICK Lesley <Lesley.MERRICK@deq.state.or.us>
2021-08-19 12:26:18 -0700

    LAM - data pulls for pH and biocriteria

---
+++ Parameters/BioCriteria/function_bio_data.R
@@ -0,0 +1,24 @@
+<- function(database) {
+  
+  
+  require(tidyverse)
+  require(RODBC)
+  require(odeqIRtools)
+  
+  print("Fetch continuous bio taxa data from IR database")
+  #connect to IR database view as a general user
+  # import bacteria data
+  #database = "IR_Dev" # use for testing 
+  IR.sql <-   odbcConnect(database)
+  
+  
+  Results_import <-    sqlFetch(IR.sql, "VW_BioCriteria") 
+ 
+  odbcClose(IR.sql)
+  
+  print(paste("Fetched", nrow(Results_import), "results from", length(unique(Results_import$MLocID)), "monitoring locations" ))
+  
+}
+
+# There is no need for data review or censored data ?? 
+  
