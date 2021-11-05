06a31b17e692b99f79af96b7cecaf45689f089e7
Travis Pritchard <pritchard.travis@deq.state.or.us>
2021-11-05 08:19:16 -0700

    Create rollup frontend to streanline rollup process.

---
+++ Rollups/rollup frontend.R
@@ -0,0 +1,36 @@
+
+#' This file will rollup individual assessments into various final products used for data display and visulalization
+#' 
+#' Mloc_rollup.R- Rolls us watershed unit monitoring locations. Produces excel document which contains 2 tabs.
+#'      - WS_MLocID_param_rollup: an accouting of all the parameter assessments done at each monitoring location
+#'      - WS_MlocID_rollup: Each monitoring location as one line with columns for IR categories base don 2022 assessments 
+#'
+#' GNIS rollup.R rolls up data into GNIS by parameter. This is what will be used to detrmine linework for the watershed 
+#' units
+#' 
+#' Other AU rollup.R rolls up non-watershed unit to an AU level
+#' together rollup.R combines watershed and other unit types. Tabs here also include beneficaial use information 
+#' 
+
+ 
+
+
+# Rollup MLOC -----------------------------------------------------------------------------------------------------
+
+source('Rollups/Mloc_rollup.R')
+
+
+# Rollup GNIS -----------------------------------------------------------------------------------------------------
+
+source('Rollups/GNIS rollup.R')
+
+
+# Rollup non WS AUs ------------------------------------------------------------------------------------------------------
+
+source('Rollups/Other AU rollup.R')
+
+
+
+# AU rollup- combined ---------------------------------------------------------------------------------------------
+
+source('Rollups/together rollup.R')
